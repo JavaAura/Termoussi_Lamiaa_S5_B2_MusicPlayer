9383ebb4506f9ef3c233b0a607edac075c88643a
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2025-01-12 16:01:36 +0100

    MS-2 separate track form component

--- src/app/features/library/library.component.ts
+++ src/app/features/library/library.component.ts
@@ -9,13 +9,14 @@ import { addTrack, loadTracks } from '../../store/actions/actions/track.actions'
 import { CommonModule } from '@angular/common';
 import { TrackService } from '../../core/services/track.service';
 import { Router } from '@angular/router';
+import { TrackFormComponent } from '../track-form/track-form.component';
 
 
 @Component({
   selector: 'app-library',
   standalone: true,
   imports: [CommonModule,
-    FormsModule],
+    FormsModule, TrackFormComponent],
   
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
@@ -23,19 +24,22 @@ import { Router } from '@angular/router';
 export class LibraryComponent {
   tracks$: Observable<Track[]> = this.store.select('tracks');
   filteredTracks$: Observable<Track[]>;
-  musicCategories = Object.values(MusicCategory);
   isAddTrackFormVisible = false;
-  newTrack: Partial<Track> = {};
   searchTerm: string = '';
   private searchTerm$: BehaviorSubject<string> = new BehaviorSubject('');
 
-  constructor(private store: Store<{ tracks: Track[] }>, private trackService: TrackService, private router: Router) {
+  constructor(
+    private store: Store<{ tracks: Track[] }>,
+    private router: Router
+  ) {
     this.filteredTracks$ = combineLatest([
       this.tracks$,
       this.searchTerm$
     ]).pipe(
       map(([tracks, searchTerm]) => {
-        return tracks.filter(track => track.title.toLowerCase().includes(searchTerm.toLowerCase()));
+        return tracks.filter(track =>
+          track.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
       })
     );
   }
@@ -56,42 +60,7 @@ export class LibraryComponent {
     this.isAddTrackFormVisible = false;
   }
 
-  onCoverSelected(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files[0]) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.newTrack.coverUrl = reader.result as string;
-      };
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-
-  onFileSelected(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files[0]) {
-      this.newTrack.audioFile = input.files[0];
-    }
-  }
-
-  addTrack() {
-    const track: Track = {
-      id: Date.now(),
-      title: this.newTrack.title!,
-      artist: this.newTrack.artist!,
-      description: this.newTrack.description,
-      duration: 3,
-      category: this.newTrack.category!,
-      addedDate: new Date(),
-      coverUrl: this.newTrack.coverUrl!,
-      audioFile: this.newTrack.audioFile
-    };
-
-    this.store.dispatch(addTrack({ track }));
-    this.closeAddTrackForm();
-  }
-
-  openTrackPage(track: any) {
+  openTrackPage(track: Track) {
     this.router.navigate([`/track/${track.id}`]);
   }
 }
--- src/app/features/track-form/track-form.component.ts
+++ src/app/features/track-form/track-form.component.ts
@@ -2,6 +2,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { MusicCategory, Track } from '../../core/models/track';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Router } from 'express';
+import { addTrack, loadTracks } from '../../store/actions/actions/track.actions';
 
 @Component({
   selector: 'app-track-form',
@@ -11,5 +15,80 @@ import { CommonModule } from '@angular/common';
   styleUrl: './track-form.component.scss'
 })
 export class TrackFormComponent {
-  
+  @Output() closeModal = new EventEmitter<void>();
+
+  musicCategories = Object.values(MusicCategory);
+  newTrack: Partial<Track> = {
+    title: '',
+    artist: '',
+    description: '',
+    category: MusicCategory.OTHER,
+    coverUrl: '',
+    audioFile: undefined 
+  };
+
+  constructor(private store: Store<{ tracks: Track[] }>) { }
+
+  closeForm() {
+    this.resetForm();
+    this.closeModal.emit();
+  }
+
+  addTrack() {
+    if (this.isFormValid()) {
+      const track: Track = {
+        id: Date.now(),
+        title: this.newTrack.title!,
+        artist: this.newTrack.artist!,
+        description: this.newTrack.description,
+        duration: 3,
+        category: this.newTrack.category!,
+        addedDate: new Date(),
+        coverUrl: this.newTrack.coverUrl!,
+        audioFile: this.newTrack.audioFile
+      };
+
+      this.store.dispatch(addTrack({ track }));
+      this.closeForm();
+    }
+  }
+
+  onCoverSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.newTrack.coverUrl = reader.result as string;
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      this.newTrack.audioFile = input.files[0];
+    }
+  }
+
+  private resetForm() {
+    this.newTrack = {
+      title: '',
+      artist: '',
+      description: '',
+      category: MusicCategory.OTHER,
+      coverUrl: '',
+      audioFile: undefined 
+    };
+  }
+
+  private isFormValid(): boolean {
+    return !!(
+      this.newTrack.title &&
+      this.newTrack.artist &&
+      this.newTrack.category &&
+      this.newTrack.coverUrl &&
+      this.newTrack.audioFile
+    );
+  }
 }
